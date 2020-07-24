8758098d4ba0593aeb9ce36aa928b923a63e4c2b
EnoshTs <enosh.tsur@haaretz.co.il>
2020-07-24 09:23:43 +0300

    initial commit

---
+++ src/main/java/com/enosh/articles/model/Author.java
@@ -0,0 +1,34 @@
+package com.enosh.articles.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "author")
+public class Author extends BaseEntity {
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "image")
+    private String image;
+
+    public Author(String firstName, String lastName, String image) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.image = image;
+    }
+}
